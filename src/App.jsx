19124c19150b7903e8d3e8e594eaac163950979c
Jeferson Conceição de Oliveira <jeferson.oliveira@salvador.ba.gov.br>
2023-10-26 16:13:15 -0300

    melhorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Mensagens } from './components/Mensagens';
-import { FormChat } from './components/FormChat';
+import { Mensagens } from "./components/Mensagens";
+import { FormChat } from "./components/FormChat";
 
 import "./global.css";
 import styles from "./App.module.css";
 
-import io from 'socket.io-client';
-const socket = io('http://localhost:3000');
+import io from "socket.io-client";
+const socket = io("http://172.22.9.169:3000");
 
 export function App() {
   const [messages, setMessages] = useState([]);
 
-  socket.on('get_messages', data => {
-      setMessages(data);     
-  })  
+  socket.on("get_messages", (data) => {
+    setMessages(data);
+  });
+
+  useEffect(() => {
+    socket.on('send_message', data => {
+      console.log(data);
+    });
+  }, []);
 
   return (
     <>
       <Header />
-        <main className={styles.container}>
-          <Mensagens messages={messages}/>
-          <FormChat socket={socket} setMessages={setMessages} messages={messages}/>
-        </main>
+      <main className={styles.container}>
+        <Mensagens messages={messages} />
+        <FormChat
+          socket={socket}
+          setMessages={setMessages}
+          messages={messages}
+        />
+      </main>
     </>
   );
 }
